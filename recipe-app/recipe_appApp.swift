45667bc825855ee96250c5d33d515ad0eb202a7a
Liam <75332524+liambsulliva@users.noreply.github.com>
2026-05-30 00:01:06 -0400

    Initial Commit

---
+++ recipe-app/recipe_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipe_appApp.swift
+//  recipe-app
+//
+//  Created by Liam Sullivan on 5/30/26.
+//
+
+import SwiftUI
+
+@main
+struct recipe_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
